2a091b86c56714cf77353bab784f45c431c1afac
jparw <jparw3@gmail.com>
2021-08-29 18:28:26 +0100

    Personal Site

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const { plugins } = require("./tailwind-default.config");
+
+module.exports = {
+  purge: [],
+  theme:{
+    extend: {
+      colors: {
+        gray: {
+          100: '#EDF2F7',
+          200: '#E2E8F0',
+          300: '#A0AEC0',
+          400: '#718096',
+          500: '#2D3748',
+         },
+         green: {
+           100: '#42b883'
+         }
+      },
+      fontFamily:{
+        inter: ['Inter']
+      }
+    },
+  },
+  plugins: [],
+  variants: {},
+}
